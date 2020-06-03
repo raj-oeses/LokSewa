6acc497b5135d1b2b2952c9b2fc50db540ee8669
WorkOut <ashishrajbanshi40@gmail.com>
2020-06-03 13:26:18 +0545

    -HomePage -Only youtube is integrated -need to use list view

---
+++ app/src/main/java/com/example/loksewanew/MCQ.java
@@ -0,0 +1,14 @@
+package com.example.loksewanew;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MCQ extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_m_c_q);
+    }
+}
---
+++ app/src/main/java/com/example/loksewanew/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.loksewanew;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        CardView youtube=findViewById(R.id.youtube);
+        CardView loksyllabus=findViewById(R.id.looksewasyllbus);
+        CardView mcq=findViewById(R.id.mcq);
+        CardView aboutus=findViewById(R.id.aboutus);
+
+        youtube.setOnClickListener(this);
+        loksyllabus.setOnClickListener(this);
+        mcq.setOnClickListener(this);
+        aboutus.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){                         //For the youtube ,, passing to next intent
+            case R.id.youtube:
+                /*Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.google.android.youtube");
+                startActivity( launchIntent );*/
+                Intent youtubeplaylist = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCudFxp2sHnBY2Q25e-3UTiw/playlists"));//link of gyane playlist
+                startActivity(youtubeplaylist);
+                break;
+            case R.id.looksewasyllbus:             //for the syllabus
+                Intent looksewasyllbaus= new Intent(this,LoksewaSyllabus.class);
+                startActivity(looksewasyllbaus);
+                break;
+            case R.id.mcq:                           //Multiple choice questions
+                Intent mcq= new Intent(this,MCQ.class);
+                startActivity(mcq);
+                break;
+            case R.id.aboutus:                        //Only displaying the content needed
+                Intent aboutus=new Intent(this,AboutUs.class);
+                startActivity(aboutus);
+                break;
+        }
+
+    }
+}
