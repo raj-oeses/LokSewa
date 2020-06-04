4ea578fc727d012da818bf2e1d8247cc7ef644f9
WorkOut <ashishrajbanshi40@gmail.com>
2020-06-04 17:14:36 +0545

    -ListView is Implemented in 1st layer -2nd layer list view need to be done -string array need to pass via intent

--- app/src/main/java/com/example/loksewanew/LoksewaSyllabus.java
+++ app/src/main/java/com/example/loksewanew/LoksewaSyllabus.java
@@ -2,7 +2,11 @@ package com.example.loksewanew;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class LoksewaSyllabus extends AppCompatActivity {
 
@@ -10,5 +14,25 @@ public class LoksewaSyllabus extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loksewa_syllabus);
+        final String[] subject = {"Aeronautical Engineering", "Architect Engineer","Automobile Engineering","Civil Engineering","Computer Engineering","Electrical Engineering","Electronics Engineering","IT Engineering","Mechanical Engineering"};
+        //final String[] pdffile = {"OperationsEngineer.pdf", "SeniorTechnicalOfficer.pdf"};
+        ListView ls = findViewById(R.id.lstview);
+        //CustomListAdaptere customListAdaptere= new CustomListAdaptere(getApplicationContext(),subject);
+        //ls.setAdapter(customListAdaptere);
+
+        // ls.setAdapter(adapter);
+        //  Normall ma default vako adapter use huntheyo
+        //  since we have made our own adapter we are using this
+        ls.setAdapter(new CustomListAdaptere(getApplicationContext(),subject));
+
+        // now we are going for the onClickListener
+        ls.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent display=new Intent(getApplicationContext(),DisplayActivity.class);
+                display.putExtra("file",subject[position]);
+                startActivity(display);
+            }
+        });
     }
 }
