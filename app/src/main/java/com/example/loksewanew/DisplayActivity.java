270b7db19993a27c23ab1cdc07c77b35e74b48ec
WorkOut <ashishrajbanshi40@gmail.com>
2020-06-06 21:33:49 +0545

    -Youtube Integration is Complete --Syllabus is done

--- app/src/main/java/com/example/loksewanew/DisplayActivity.java
+++ app/src/main/java/com/example/loksewanew/DisplayActivity.java
@@ -2,7 +2,10 @@ package com.example.loksewanew;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.github.barteksc.pdfviewer.PDFView;
 
 public class DisplayActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class DisplayActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
+        Intent message=getIntent();
+        String nayapdf=message.getStringExtra("final");
+
+        PDFView display= (PDFView) findViewById(R.id.display);
+        display.fromAsset(nayapdf+".pdf").load();
     }
 }
